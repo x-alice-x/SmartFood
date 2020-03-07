76d26becb03ce5be393ff1fcaba74e9ebf445b44
Alex <dr.grechka@gmail.com>
2020-03-07 19:29:06 +0700

    swiperFix

--- src/store/dishes.js
+++ src/store/dishes.js
@@ -30,9 +30,17 @@ export default {
             }
             axios(requestParams)
                 .then(resp => {
+                    if(resp.data.data){
+                        for (let i = 0; i < resp.data.data.length; i++){
+                            resp.data.data[i].dishes.forEach((item) => {
+                                item.swipe = 'middle'
+                                return item})
+                        }
                         dishes = resp.data.data
+                        console.log(resp.data.data)
                         commit('updateDates', dishes)
                         commit('setDishes')
+                    }
                     },
                     err => {
                         console.log(err)
